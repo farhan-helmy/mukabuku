89eacf3dece8e485366ca4eebb1cd9e90ec6f321
asyrxfjm <asyrafirfan0007@gmail.com>
2021-05-09 14:02:48 +0800

    bring theme assets and modify login page

---
+++ resources/views/auth/app.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" type="image/png" href="assets/img/favicon.png" />
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="stylesheet" href="assets/css/core.css">
+</head>
+
+<body>
+
+    <!-- Pageloader -->
+    <div class="pageloader"></div>
+    <div class="infraloader is-active"></div>
+    <div class="signup-wrapper">
+
+        <!--Fake navigation-->
+        <div class="fake-nav">
+            <a href="/" class="logo">
+                <img class="light-image" src="assets/img/logo/friendkit-bold.svg" width="112" height="28" alt="">
+                <img class="dark-image" src="assets/img/logo/friendkit-white.svg" width="112" height="28" alt="">
+            </a>
+        </div>
+
+        <div class="container">
+            <!--Container-->
+            @yield('content')
+        </div>
+    </div>
+
+    <!-- Concatenated js plugins and jQuery -->
+    <script src="assets/js/app.js"></script>
+    <script src="https://js.stripe.com/v3/"></script>
+    <script src="assets/data/tipuedrop_content.js"></script>
+
+    <!-- Core js -->
+    <script src="assets/js/global.js"></script>
+
+    <!-- Navigation options js -->
+    <script src="assets/js/navbar-v1.js"></script>
+    <script src="assets/js/navbar-v2.js"></script>
+    <script src="assets/js/navbar-mobile.js"></script>
+    <script src="assets/js/navbar-options.js"></script>
+    <script src="assets/js/sidebar-v1.js"></script>
+
+    <!-- Core instance js -->
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/chat.js"></script>
+    <script src="assets/js/touch.js"></script>
+    <script src="assets/js/tour.js"></script>
+
+    <!-- Components js -->
+    <script src="assets/js/explorer.js"></script>
+    <script src="assets/js/widgets.js"></script>
+    <script src="assets/js/modal-uploader.js"></script>
+    <script src="assets/js/popovers-users.js"></script>
+    <script src="assets/js/popovers-pages.js"></script>
+    <script src="assets/js/lightbox.js"></script>
+
+    <!-- Landing page js -->
+
+    <!-- Signup page js -->
+
+    <!-- Feed pages js -->
+
+
+    <!-- profile js -->
+
+    <!-- stories js -->
+
+    <!-- friends js -->
+
+    <!-- questions js -->
+
+    <!-- video js -->
+
+    <!-- events js -->
+
+    <!-- news js -->
+
+    <!-- shop js -->
+
+    <!-- inbox js -->
+
+    <!-- settings js -->
+
+
+</body>
+
+</html>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,67 @@
-@extends('layouts.app')
+@extends('auth.app')
 
 @section('content')
-<div class="container">
+
+<div class="login-container">
+    <div class="columns is-vcentered">
+        <div class="column is-6 image-column">
+            <!--Illustration-->
+            <img class="light-image login-image" src="assets/img/illustrations/login/login.svg" alt="">
+            <img class="dark-image login-image" src="assets/img/illustrations/login/login-dark.svg" alt="">
+        </div>
+        <div class="column is-6">
+
+            <h2 class="form-title">Welcome Back</h2>
+            <h3 class="form-subtitle">Enter your credentials to sign in.</h3>
+
+            <!--Form-->
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="login-form">
+                    <div class="form-panel">
+                        <div class="field">
+                            <label>Email</label>
+                            <div class="control">
+                                <input id="email" type="email" class="input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label>Password</label>
+                            <div class="control">
+                                <input id="password" type="password" class="input @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            </div>
+                        </div>
+                        <div class="field is-flex">
+                            <div class="switch-block">
+                                <label class="f-switch">
+                                    <input class="is-switch" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <i></i>
+                                </label>
+                                <div class="meta">
+                                    <p>Remember me?</p>
+                                </div>
+                            </div>
+                            @if (Route::has('password.request'))
+                                <a href="{{ route('password.request') }}">
+                                    Forgot Your Password?
+                                </a>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="buttons">
+                        <button type="submit" class="button is-solid primary-button is-fullwidth raised">Login</button>
+                    </div>
+
+                    <div class="account-link has-text-centered">
+                        <a href="/signup.html">Don't have an account? Sign Up</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +129,7 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
+
+
